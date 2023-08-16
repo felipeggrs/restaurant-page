0c9f8a7074669ac1831e5b0425f6678526cbaf0c
felipe <felipeggrs@gmail.com>
2023-08-16 12:18:11 -0300

    Add headline, image and basic paragraph

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import "./styles.css";
+import Background from "./bg.jpg";
+
+const imgContainer = document.getElementById("bgImg");
+imgContainer.setAttribute("style", `background-image: url(${Background})`);
+
 console.log("The index.js file and webpack are working correctly!");
